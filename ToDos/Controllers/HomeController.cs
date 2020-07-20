3c027d233ef36eb59be06898dc480b7a5c831b98
Chris Yoon <chy.yoon@gmail.com>
2020-07-20 16:04:15 -0700

    seperated controllers and views
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- ToDos/Controllers/HomeController.cs
+++ ToDos/Controllers/HomeController.cs
@@ -1,31 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
-using ToDos.Models;
-using System.Collections.Generic;
 
 namespace ToDos.Controllers
 {
-   public class HomeController : Controller
+  public class HomeController : Controller
   {
 
     [HttpGet("/")]
     public ActionResult Index()
     {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
-    {
       return View();
     }
-
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
-    }
-
   }
 }
