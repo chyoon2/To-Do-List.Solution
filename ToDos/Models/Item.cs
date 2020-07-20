95917945c4ed46752fd7124435622e2f3ef8ed47
Chris Yoon <chy.yoon@gmail.com>
2020-07-20 15:44:02 -0700

    add list and loop to index
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- ToDos/Models/Item.cs
+++ ToDos/Models/Item.cs
@@ -1,26 +1,27 @@
 using System.Collections.Generic;
+
 namespace ToDos.Models
 {
   public class Item
   {
-
     public string Description { get; set; }
     private static List<Item> _instances = new List<Item> {};
+
+    public Item (string description)
+    {
+      Description = description;
+      _instances.Add(this);
+    }
 
     public static List<Item> GetAll()
     {
       return _instances;
     }
 
-    public Item(string description)
-    {
-      Description = description;
-      _instances.Add(this);
-    }
-
     public static void ClearAll()
     {
       _instances.Clear();
     }
+
   }
 }
