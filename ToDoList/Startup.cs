17f047846461c4ddd2c2f2235f131ce0e520d815
Chris Yoon <chy.yoon@gmail.com>
2020-07-28 10:18:32 -0700

    add todolistcontext file and altered itemscontroller
    Co-Authored-By: Micheal Hansen <micheal.hansen@gmail.com>

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.Models;
 
 namespace ToDoList
 {
@@ -12,17 +14,20 @@ namespace ToDoList
     {
       var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
+          .AddJsonFile("appsettings.json");
       Configuration = builder.Build();
     }
 
-    public IConfigurationRoot Configuration { get; }
+    public IConfigurationRoot Configuration { get; set;}
 
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+    
+    services.AddEntityFrameworkMySql()
+      .AddDbContext<ToDoListContext>(options => options
+      .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
     }
-
     public void Configure(IApplicationBuilder app)
     {
       app.UseStaticFiles();
@@ -41,10 +46,5 @@ namespace ToDoList
         await context.Response.WriteAsync("Something went wrong!");
       });
     }
-  }
-
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
   }
 }
