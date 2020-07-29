07ce54b6458aac7ba11e552bf7d1c6d8e0953ee2
Chris Yoon <chy.yoon@gmail.com>
2020-07-29 11:07:20 -0700

    add functionality to project
    Co-authored-by: Ben White <bwhite2140@outlook.com>
    > Co-authored-by: Tyson Lackey <lackeyt90@gmail.com>

--- ToDoList/Controllers/CategoriesControllers.cs
+++ ToDoList/Controllers/CategoriesControllers.cs
@@ -37,6 +37,7 @@ namespace ToDoList.Controllers
     public ActionResult Details(int id)
     {
       Category thisCategory = _db.Categories.FirstOrDefault(category => category.CategoryId == id);
+      Viewbag.ItemId = new List(_db.Items, "ItemId", "Name")
       return View(thisCategory);
     }
 
